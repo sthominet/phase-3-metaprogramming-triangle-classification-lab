240403be4e93bd63ef50307d55ceaac45b2e40de
Sandrine <sandrine.thominet@gmail.com>
2022-07-29 15:47:41 -0400

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :sides
+
+  def initialize(a, b, c)
+    @sides = a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+  
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
